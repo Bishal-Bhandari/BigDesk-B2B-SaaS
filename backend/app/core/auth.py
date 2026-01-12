a9e59fc97c0570fa0c0bb5906155d5ee4856b950
bishal <bishalbhaandari62@gmail.com>
2026-01-12 18:11:44 +0100

    add AuthUser class to manage user authentication details

--- backend/app/core/auth.py
+++ backend/app/core/auth.py
@@ -3,3 +3,10 @@ from fastapi import HTTPException, status, Depends, Request
 from clerk_backend_api.security import AuthenticateRequestOptions
 from config import settings
 from clerk import clerk
+
+class AuthUser:
+    def __init__(self, user_id: str, org_id: str, org_permissions: list):
+        self.user_id = user_id
+        self.org_id = org_id
+        self.org_permissions = org_permissions
+
