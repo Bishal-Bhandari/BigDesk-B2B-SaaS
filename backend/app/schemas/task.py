165b5c2f5074e727722995022ce6967d38a1a37d
bishal <bishalbhaandari62@gmail.com>
2026-01-14 12:21:27 +0100

    add Task model with attributes for task management

---
+++ backend/app/schemas/task.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel
+from backend.app.models.task import TaskStatus
+
+class taskCreate(BaseModel):
+    title: str
+    description: Optional[str] = None
+    status: TaskStatus = TaskStatus.PENDING
+
+class taskUpdate(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    status: Optional[TaskStatus] = None
+
+class TaskStatusupdate(BaseModel):
+    status: TaskStatus
+
+class TaskResponse(BaseModel):
+    id: int
+    title: str
+    description: Optional[str] = None
+    status: TaskStatus
+    org_id: int
+    created_by: str
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        from_attributes = True
