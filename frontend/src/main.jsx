4a50f3a6bdb49bce7fa2f1b70f2bfc56f0853174
bishal <bishalbhaandari62@gmail.com>
2026-01-18 17:29:49 +0100

    -app in clerk provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ClerkProvider } from '@clerk/clerk-react'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
@@ -11,6 +13,10 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+          <BrowserRouter>
+             <App />
+          </BrowserRouter>
+      </ClerkProvider
   </StrictMode>,
 )
