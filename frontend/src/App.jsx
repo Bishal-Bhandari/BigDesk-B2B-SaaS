b53ebcc9e3d79e17ebf608d1bcc141a0990116e5
bishal <bishalbhaandari62@gmail.com>
2026-01-20 19:28:29 +0100

    -Import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,10 @@
-
+import {Routes, Route} from 'react-router-dom';
+import {SignIn, SignOut, RedirectTOSignIn} from "@clerk/clerk-react";
+import HomePage from "./pages/HomePage.jsx";
+import SignInPage from "./pages/SignInPage.jsx";
+import SignUpPage from "./pages/SignUp.jsx";
+import DashboardPage from "./pages/DashboardPage.jsx";
+import PricingPage from "./pages/PricingPage.jsx";
 
 function App() {
 
