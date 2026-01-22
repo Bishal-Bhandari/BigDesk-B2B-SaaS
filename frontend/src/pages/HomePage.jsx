135211db8e9ed0dc5bcb19183f2c4a8f8527d8a8
bishal <bishalbhaandari62@gmail.com>
2026-01-22 20:12:14 +0100

    Setting up home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,35 @@
+import {Link} from "react-router-dom"
+import {SignedIn, SignedOut, useOrganization, CreateOrganization} from "@clerk/clerk-react";
+
 function HomePage(){
 
-    return <></>
+    return <div className={"home-container"}>
+        <h1 className={"home-title"}>
+            Welcome to Nira Task Management
+            <span className={"home-title-accent"}> Made Simple. </span>
+        </h1>
+        <p className={"home-subtitle"}>
+            Streamline your tasks and boost productivity with our intuitive task management solution.
+        </p>
+
+        <SignedOut>
+            <div className={"home-buttons"}>
+                <Link to={"/sign-up"} className={"btn btn-primary btn-lg"}>
+                    Start for free.
+                </Links>
+                <Link to={"/sign-in"} className={"btn btn-outline btn-lg"}>
+                    Sign In
+                </Links>
+            </div>
+        </SignedOut>
+        <SignedIn>
+            {organization ? (
+                <Link to={"/dashboard"} className={"btn btn-primary btn-lg"}>
+                    Go to Dashboard
+                </Link>
+                )}
+        </SignedIn>
+    </div>
 
     }
 
