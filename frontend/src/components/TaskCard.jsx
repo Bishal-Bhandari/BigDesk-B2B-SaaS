8c068e5c3463337864a2ec32520766b5a76b5fce
bishal <bishalbhaandari62@gmail.com>
2026-01-27 12:47:45 +0100

    debugging

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -4,7 +4,7 @@ function TaskCard({task, onEdit, onDelete}){
 
     return <div
         className={`task-card ${canEdit ? 'task-card-clickable': ""}`}
-        onClick={canEdit ? => onEdit(task) : undefined}
+        onClick={canEdit ? () => onEdit(task) : undefined}
     >
         <div className={"tsk-card-header"}>
             <h4 className={"task-card-title"}>{task.title}</h4>
