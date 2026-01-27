c3598c2a152dc181a19f550c511499320ae6f863
bishal <bishalbhaandari62@gmail.com>
2026-01-27 11:32:36 +0100

    dashboard display jsx

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,4 +1,18 @@
+import {useState, useEffect, useCallback} from "react"
+import {useAuth, useOrganization, CreateOrganization} from "@clerk/clerk-react";
+import {getTasks} from "../services/api"
+import KanbanBoard from "../components/KanbanBoard.jsx"
+
 function DashboardPage(){
+    const {getToken} = useAuth()
+    const {organization, membership} = useOrganization(
+        {membership: {infinite: true}}
+        )
+    const [tasks, setTasks] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    const
 
     return <></>
 
